bbf7b00afc1f0d1240785bc1a359f9a8993a9b6d
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-05-28 12:24:10 -0700

    edits to helper functions and gpt prompts

--- scripts/get_yc_description.py
+++ scripts/get_yc_description.py
@@ -1,69 +1,55 @@
 import os
-import datetime 
 import pandas as pd 
+from datetime import datetime, timedelta 
 
 import openai 
-
 from google.cloud import bigquery 
 
+import utils 
 
 DATA_CLEANED_DIR = "./data/cleaned/yield_curve_historical_rates_MASTER.parquet"
-CURRENT_DATE = str(datetime.datetime.today()).split(" ")[0]
+CURRENT_DATE = utils.clean_date(datetime.today())
 
-PROMPT = f"""
-    **US Treasury Yield Curve Movements On {CURRENT_DATE}**
+df = pd.read_parquet(DATA_CLEANED_DIR)
+summary_str = utils.summary_data_str(DATA_CLEANED_DIR)
+spy_data = utils.get_historical_market_data() 
 
-    **Overview:**
-    <PROVIDE A BRIEF OVERVIEW OF THE CURRENT STATE OF THE YIELD CURVE AND THE MOST RECENT YIELD CURVE DYNAMICS THAT WOULD BE RELEVANT TO ECONOMISTS AND INVESTORS. COMMENT ON IF THE CURVE STEEPEND OR FLATTENED FOR EXAMPLE.>
+OVERVIEW_PROMPT = f""" 
 
-    **What it Means**
-    1. **Economic Outlook:** <ONE TO THREE SENTENCES ON THE ECONOMIC OUTLOOK BASED ON THE YIELD CURVE>
-    
-    2. **Monetary Policy:** <ONE TO THREE SENTENCES ON WHAT THE CURRENT YIELD CURVE MIGHT INDICATE FOR MONETARY POLICY>
+    Today is {CURRENT_DATE}. \
+    Provide a brief 5 sentence commentary on the current dynamics of the US Treasuy Yield Curve based on your analysis of this summary data: {summary_str},\
+    and this data from the last month of end-of-day yield curve values: {df.iloc[: 31].to_string(index = False)}. \
+    To aid in your commentary is the last month of SPY ETF data: {spy_data.to_string(index = False)}
 
-    3. **Investment Strategies:** <GIVE A BRIEF COMMENT ON WHAT THE CURRENT YIELD CURVE MIGHT INDICATE FOR INVESTORS>
+    Your analysis and comentary should be intelligent and not juvenile, non-speculative, and not contain financial advice. It should be easily understood by an 8th grader, but compelling \
+    enough such that a professional investor would find reading it worthwhile. You may also indicate other areas researchers may wish to study for more context on the macroeconomic environment.
+
+    I gill give you $500 dollars if you can accomplish this. Be sure to utilize the active voice when referencing any data or statistics.
+
 """
 
-def generate_insight(data_summary: pd.DataFrame) -> str:
-    # get model response based on the most current data
+# Create a function to generate insights
+def generate_insight() -> str:
     response = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
-            {"role": "system", "content": "You are an economic expert on the US Treasury Yield Curve. \
-                                           You provide intelligent, objective, non-speculative analyses of recent data.\
-                                           You provide these analyses daily after the closing values of the Yield Curve are determined."},
-            {"role": "user", "content": f"Provide insights based on the following data in the format below.\
-                                          Fill in your analyses in the carrots <>:\
-                                          The response template you need to use: {PROMPT}. \
-                                          The summary data: {data_summary}."}
+            {"role": "system", "content": "You are an expert on the US Treasury Yield Curve. You provide concise daily commentary on the Yield Curve's recent movements."},
+            {"role": "user", "content": f"Please answer the following prompt: {OVERVIEW_PROMPT}"}
         ]
     )
     return response['choices'][0]['message']['content'].strip()
 
-def push_to_big_query(data):
-    # init client 
-    client = bigquery.Client()
-
-    # Define your dataset and table
-    dataset_id = 'yieldcurve'
-    table_id = 'daily_description'
-
-    table_ref = client.dataset(dataset_id).table(table_id)
-    # Insert data into the table
-    to_insert = [
-        data
-    ]
-    errors = client.insert_rows_json(table_ref, to_insert)  # Make an API request.
-    if errors == []:
-        print("New rows have been added.")
-    else:
-        print("Encountered errors while inserting rows: {}".format(errors))
-
 
 if __name__ == "__main__": 
 
-    # Get insights for the DataFrame summary
-    df = pd.read_parquet(DATA_CLEANED_DIR)
-    insights = generate_insight(data_summary=df.iloc[: 31])
-    data = {"Date": CURRENT_DATE, "Description": insights}
-    push_to_big_query(data)
+    if utils.is_trading_day(): 
+        temp = generate_insight()
+        insights = f"Summary Statistics on {CURRENT_DATE}\n{summary_str}\n\nMarket Overview\n\n{temp}"
+    else: 
+        insights = f"Market Overview on {CURRENT_DATE}\n\nMarkets are closed today. Please check back for updated insights soon."
+
+    data = {
+        "Date": CURRENT_DATE, 
+        "Description": insights
+    }
+    utils.push_to_big_query(data)
--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,110 @@
-# helper functions to use in streamlit_app.py
+# helper functions for: 
+#   - get_yc_data.py
+#   - get_yc_description.py
 
-def date_filter_format(date): 
-    return "-".join([str(date.year), str(date.month), str(date.day)])
+import pandas as pd 
+from google.cloud import bigquery 
+from datetime import datetime, timedelta
+import pandas_market_calendars as mcal
+import yfinance as yf 
+
+
+def clean_date(datetime_date): 
+    # returns cleaned datetime date as str 
+    return datetime_date.strftime('%Y-%m-%d')
+
+
+def is_trading_day() -> bool: 
+    """ 
+        Determines if current day is a trading day on NYSE
+        Returns True if it is a trading day, else False. 
+    """
+    nyse = mcal.get_calendar('NYSE')
+    today = datetime.today()
+    schedule = nyse.schedule(start_date = today, end_date = today)
+    return False if schedule.empty else True 
+
+
+def get_historical_market_data(ticker_symbol = "SPY"): 
+    # fetches historical ticker data for the past month
+    today = datetime.today()
+    past = today - timedelta(days = 31)
+    # Fetch the historical data
+    spy_data = yf.download(ticker_symbol, start=clean_date(past), end=clean_date(today), interval='1d').reset_index() 
+    # return only the necessary columns since these are passed into prompt (don't need excess tokens being processed)
+    return spy_data[["Date", "Adj Close"]]
+
+
+def summary_data_query(): 
+    # init client 
+    client = bigquery.Client() 
+    # Define your dataset and table
+    dataset_id = 'yieldcurve'
+    table_id = 'historical'
+    table_ref = client.dataset(dataset_id).table(table_id) 
+
+    query = f""" 
+        WITH inversion_date AS (
+            SELECT 
+                MAX(CAST(Date AS DATE)) AS last_inversion_date
+            FROM 
+                {table_ref} 
+            WHERE 
+                `2_Yr` < `10_Yr`
+        ),
+        contango_data AS (
+            SELECT 
+                *
+            FROM 
+                {table_ref}
+            ORDER BY 
+                Date DESC 
+            LIMIT 1
+        )
+        SELECT 
+            (SELECT CAST(1 + last_inversion_date AS string) FROM inversion_date) AS last_inversion_date,
+            (SELECT 1 + DATE_DIFF(CURRENT_DATE(), last_inversion_date, DAY) FROM inversion_date) AS num_days_since_last_inversion, 
+            (SELECT ROUND((`3_Mo` - `10_Yr`) / `3_Mo`, 2) FROM contango_data) AS contango_3m_10y, 
+            (SELECT ROUND(`3_Mo` - `10_Yr`, 2) FROM contango_data) AS diff_3m_10y, 
+            (SELECT ROUND((`2_Yr` - `10_Yr`) / `2_Yr`, 2) FROM contango_data) AS contango_2y_10y,
+            (SELECT ROUND(`2_Yr___10_Yr`, 2) FROM contango_data) AS diff_2y_10y, 
+        ;
+    """
+    results = client.query(query)  
+    summary_data = pd.DataFrame([dict(_) for _ in results.result()])
+    return summary_data
+
+
+def summary_data_str(data_cleaned_dir): 
+    summary_data = summary_data_query() 
+    df = pd.read_parquet(data_cleaned_dir)
+    summary_str = f"""
+        **Is inverted**: {True if df.iloc[0]["2 Yr"] > df.iloc[0]["10 Yr"] else False}
+        **Last inversion date**: {summary_data["last_inversion_date"].values[0]}
+        **Days since last inversion**: {summary_data["num_days_since_last_inversion"].values[0]}
+        **2 Year-10 Year Difference**: {summary_data["diff_2y_10y"].values[0]}%
+        **3 Month-10 Year Difference**: {summary_data["diff_3m_10y"].values[0]}%
+    """
+    return summary_str
+
+
+def push_to_big_query(data):
+    # init client 
+    client = bigquery.Client()
+
+    # Define your dataset and table
+    dataset_id = 'yieldcurve'
+    table_id = 'daily_description'
+
+    table_ref = client.dataset(dataset_id).table(table_id)
+    # Insert data into the table
+    to_insert = [
+        data
+    ]
+    errors = client.insert_rows_json(table_ref, to_insert)  # Make an API request.
+    if errors == []:
+        print("New rows have been added.")
+    else:
+        print("Encountered errors while inserting rows: {}".format(errors))
+
+
