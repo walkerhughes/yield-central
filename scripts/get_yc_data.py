eb95e0bc45742d08677c8f9ff2c99dc720cb11b6
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-06-03 14:01:34 -0700

    edits to utils file path

--- scripts/get_yc_data.py
+++ scripts/get_yc_data.py
@@ -1,4 +1,4 @@
-from scraper import Scraper
+from scripts.utils.get_daily_data_scraper import Scraper
 
 def main(): 
     try: 
--- scripts/get_yc_description.py
+++ scripts/get_yc_description.py
@@ -1,37 +1,47 @@
-import os
 import pandas as pd 
-from datetime import datetime, timedelta 
+from datetime import datetime
 
-import openai 
-from google.cloud import bigquery 
+import scripts.utils.get_daily_discription as get_daily_discription 
 
-import utils 
-
-CURRENT_DATE = utils.clean_date(datetime.today())
+CURRENT_DATE = get_daily_discription.clean_date(datetime.today())
 DATA_CLEANED_DIR = "./data/cleaned/yield_curve_historical_rates_MASTER.parquet"
 
 yc_data = pd.read_parquet(DATA_CLEANED_DIR)
-summary_str = utils.summary_data_str(DATA_CLEANED_DIR)
-spy_data = utils.get_historical_market_data() 
+summary_str = get_daily_discription.summary_data_str(DATA_CLEANED_DIR)
+spy_data = get_daily_discription.get_historical_market_data() 
 
-overview_prompt, citations = utils.get_prompt(
-    date=CURRENT_DATE, 
-    summary_data=summary_str, 
-    historical_yc=yc_data.iloc[: 31].to_string(index = False), 
-    historical_spy=spy_data.to_string(index = False)
+overview_prompt, citations = get_daily_discription.get_prompt(
+    date = CURRENT_DATE, 
+    summary_data = summary_str, 
+    historical_yc = yc_data.iloc[: 31].to_string(index = False), 
+    historical_spy = spy_data.to_string(index = False)
 )
 
 if __name__ == "__main__": 
 
-    if utils.is_trading_day(): 
-        temp = utils.generate_insight(overview_prompt)
-        tldr = utils.generate_tldr(temp)
-        insights = f"\n**TL;DR**\n\n{tldr}\n\n**Digging Deeper**\n\n{temp}\n\n{citations}"
+    if get_daily_discription.is_trading_day(): 
+
+        temp_insights = get_daily_discription.generate_insight(overview_prompt)
+        insights = f"\n**Digging Deeper**\n\n{temp_insights}\n\n{citations}"
+
+        temp_tldr = get_daily_discription.generate_tldr(temp_insights)
+        tldr = f"\n**TL;DR**\n\n{temp_tldr}\n"
+        
     else: 
         insights = f"\nMarkets are closed today. Please check back for updated insights soon."
+        tldr = "\nValues displayed are from last trading day."
 
-    data = {
-        "Date": CURRENT_DATE, 
-        "Description": insights
-    }
-    utils.push_to_big_query(data)
+    get_daily_discription.push_to_big_query(
+        {
+            "Date": CURRENT_DATE, 
+            "Description": insights
+        },
+        table_id = 'daily_description'
+    ) 
+    get_daily_discription.push_to_big_query(
+        {
+            "Date": CURRENT_DATE, 
+            "TLDR": tldr
+        },
+        table_id = 'tldr'
+    )
