39cc2538559f38d120b36e1cf6cd8225613e24e8
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-06-04 14:49:06 -0700

    edit to push article citations to separate table in BigQuery

--- scripts/get_yc_description.py
+++ scripts/get_yc_description.py
@@ -7,31 +7,34 @@ import utils.get_news_articles as get_news_articles
 CURRENT_DATE = get_daily_discription.clean_date(datetime.today())
 DATA_CLEANED_DIR = "./data/cleaned/yield_curve_historical_rates_MASTER.parquet"
 
-# get intermediate data to pass into prompt 
-yc_data = pd.read_parquet(DATA_CLEANED_DIR)
-summary_str = get_daily_discription.summary_data_str(DATA_CLEANED_DIR)
-spy_data = get_daily_discription.get_historical_market_data() 
-
-data = get_news_articles.get_alphavantage_articles()
-top_k_articles = get_news_articles.get_top_k_relevant_articles(data, 3) 
-article_summaries = get_news_articles.get_top_k_summaries(top_k_articles)
-citations = get_news_articles.get_top_k_citations(top_k_articles)
-
-# get prompt for generating daily insights 
-prompt = get_daily_discription.get_prompt(
-    date = CURRENT_DATE, 
-    summary_data = summary_str, 
-    historical_yc = yc_data.iloc[: 31].to_string(index = False), 
-    historical_spy = spy_data.to_string(index = False),
-    article_summaries = article_summaries
-)
 
 if __name__ == "__main__": 
 
+    # generate insights only if its a trading day 
     if get_daily_discription.is_trading_day(): 
 
+        # get intermediate data to pass into prompt 
+        yc_data = pd.read_parquet(DATA_CLEANED_DIR)
+        summary_str = get_daily_discription.summary_data_str(DATA_CLEANED_DIR)
+        spy_data = get_daily_discription.get_historical_market_data() 
+
+        # fetch today's relevant news articles and their citations 
+        data = get_news_articles.get_alphavantage_articles()
+        top_k_articles = get_news_articles.get_top_k_relevant_articles(data, 3) 
+        article_summaries = get_news_articles.get_top_k_summaries(top_k_articles)
+        citations = get_news_articles.get_top_k_citations(top_k_articles)
+
+        # create prompt for generating daily insights 
+        prompt = get_daily_discription.get_prompt(
+            date = CURRENT_DATE, 
+            summary_data = summary_str, 
+            historical_yc = yc_data.iloc[: 31].to_string(index = False), 
+            historical_spy = spy_data.to_string(index = False),
+            article_summaries = article_summaries
+        )
+
         temp_insights = get_daily_discription.generate_insight(prompt)
-        insights = f"\n{temp_insights}\n\n{citations}"
+        insights = f"\n{temp_insights}\n\n"
 
         temp_tldr = get_daily_discription.generate_tldr(temp_insights)
         tldr = f"\n**TL;DR**\n\n{temp_tldr}\n"
@@ -39,6 +42,7 @@ if __name__ == "__main__":
     else: 
         insights = f"\nMarkets are closed today. Please check back for updated insights soon."
         tldr = "\nValues displayed are from last trading day."
+        citations = ""
 
     get_daily_discription.push_to_big_query(
         {
@@ -53,4 +57,11 @@ if __name__ == "__main__":
             "TLDR": tldr
         },
         table_id = 'tldr'
+    )
+    get_daily_discription.push_to_big_query(
+        {
+            "Date": CURRENT_DATE, 
+            "Citations": citations
+        },
+        table_id = 'citations'
     )
